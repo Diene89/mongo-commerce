0692eed91dd36fc3661d6292c34b52a5f8d14807
Diene89 <diene_89@hotmail.com>
2022-09-08 20:55:50 -0300

    teste

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -2,4 +2,4 @@ db.resumoProdutos.insertOne({
     franquia: "McDonalds",
     totalProdutos: db.produtos.countDocuments(),
   });
-  db.resumoProdutos.find({}, { franquia: 1, totalProdutos: 1, _id: 0 });
+db.resumoProdutos.find({ franquia: "McDonalds" }, { franquia: 1, totalProdutos: 1, _id: 0 });
